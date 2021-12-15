e117ff85f25bb541851a5e31908a245629c4953d
jin-co <lilijk@hanmail.net>
2021-12-15 19:38:16 +0900

    chore: update

--- Console/KBaekAssignment4/src/add/console/Main.java
+++ Console/KBaekAssignment4/src/add/console/Main.java
@@ -1,10 +1,36 @@
 package add.console;
 
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hi");
-        int intMax = Integer.MAX_VALUE;
-        int intMin = Integer.MIN_VALUE;
+        TextFileReader reader = new TextFileReader();
+        List<String> lines = reader.readFromProjectPath("words_250.txt");
+
+        // writing to console
+//        lines.forEach(System.out::println);
+//        System.out.println(lines);
+//        lines.forEach(line -> System.out.println());
+        for (String line: lines) {
+            System.out.println(line);
+        }
+
+        System.out.println("------------------------------\n" +
+                "words after removing special characters\n" +
+                "------------------------------");
+        for (var line: lines) {
+            String wordsWithoutCharacters = line.replaceAll("[^a-zA-Z]+", "");
+            System.out.println(wordsWithoutCharacters);
+        }
+
+        System.out.println("------------------------------\n" +
+                "length -- number of words\n" +
+                "------------------------------");
+
+        for (var line: lines) {
+            int length = line.length();
+            System.out.println();
+        }
     }
 }
---
+++ Console/KBaekAssignment4/src/add/console/TextFileReader.java
@@ -0,0 +1,33 @@
+package add.console;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.logging.Logger;
+
+public class TextFileReader {
+    Logger log = Logger.getLogger(this.getClass().getName());
+     public List<String> readFromProjectPath (String fileName) {
+         List<String> lines = new ArrayList<>();
+         String projectPath = "src/data/director/" + fileName;
+         // using path to create a file
+         File file = new File(projectPath);
+         // getting full path
+         String fullPath = file.getAbsolutePath();
+
+         try {
+             BufferedReader reader = new BufferedReader(new FileReader(fullPath));
+             String line;
+             while ((line = reader.readLine()) != null) {
+                 lines.add(line);
+             }
+             reader.close();
+         } catch (FileNotFoundException e) {
+             log.severe(e.getMessage());
+         } catch (IOException e) {
+             log.severe(e.getStackTrace().toString());
+         }
+
+         return  lines;
+     }
+}
