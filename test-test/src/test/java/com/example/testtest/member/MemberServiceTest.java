3bd3ca40df552e0afc9e5503166a5fbe09e62301
jin-co <lilijk@hanmail.net>
2023-08-06 15:29:55 +0900

    Completed member test

---
+++ test-test/src/test/java/com/example/testtest/member/MemberServiceTest.java
@@ -0,0 +1,18 @@
+package com.example.testtest.member;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+
+public class MemberServiceTest {
+    MemberService memberService = new MemberServiceImpl();
+
+    @Test
+    void join() {
+        Member member = new Member(1L, "Tom", Grade.VIP);
+        memberService.join(member);
+        Member findMember = memberService.findMember(1L);
+        Assertions.assertThat(member).isEqualTo(findMember);
+    }
+}
