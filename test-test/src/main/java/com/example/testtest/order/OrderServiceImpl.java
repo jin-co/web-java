51fa2accd4f3b6f80f98a207df2740dc71619774
Kwangjin Baek <lilijk@hanmail.net>
2023-08-16 13:38:27 +0900

    Clean

--- test-test/src/main/java/com/example/testtest/order/OrderServiceImpl.java
+++
@@ -1,9 +0,0 @@
-package com.example.testtest.order;
-
-public class OrderServiceImpl implements OrderService {
-
-    @Override
-    public Order createOrder(Long orderId, String orderName, int price, int discount) {
-        return new Order(orderId, orderName, price, discount);
-    }
-}
