a8c0f4c2388377f4a2267dded94073f0ab6ed67a
Kwangjin Baek <lilijk@hanmail.net>
2023-08-16 15:37:55 +0900

    Completed

---
+++ test-test/src/main/java/com/example/testtest/discount/FixedDiscount.java
@@ -0,0 +1,16 @@
+package com.example.testtest.discount;
+
+import com.example.testtest.member.Grade;
+import com.example.testtest.member.Member;
+
+public class FixedDiscount implements DiscountPolicy {
+    private final int fixedDiscount = 1000;
+    @Override
+    public int getDiscount(Member member, int price) {
+        if(member.getGrade() == Grade.VIP) {
+            return fixedDiscount;
+        } else {
+            return 0;
+        }
+    }
+}
---
+++ test-test/src/main/java/com/example/testtest/order/OrderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.example.testtest.order;
+
+public class OrderServiceImpl implements OrderService {
+    @Override
+    public Order createOrder(Long orderId, String orderName, int price, int discount) {
+        return new Order(orderId, orderName, price, discount);
+    }
+}
