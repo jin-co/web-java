2c43cd2a31c470ba7aceb852a6ceca3aab5df8bd
jin-co <lilijk@hanmail.net>
2023-08-29 22:14:49 +0900

    Update

--- thymeleaf/src/main/java/com/example/thymeleaf/controller/ThymeleafController.java
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/ThymeleafController.java
@@ -5,12 +5,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.Mapping;
 
+import java.util.List;
+
 @Controller
 public class ThymeleafController {
 
     @GetMapping("/home")
     public String homeMain(Model model) {
         model.addAttribute("name", "dynamic");
+        model.addAttribute("elvis", false);
+        List<Integer> list = List.of(1, 2, 3);
+        model.addAttribute("iteration", list);
         return "home";
     }
 }
