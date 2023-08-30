3063bbde4ed318c3a27073dc37c742f119e8a055
Kwangjin Baek <lilijk@hanmail.net>
2023-08-30 13:11:42 +0900

    Completed employee local app

---
+++ test-employee/src/main/java/com/example/testemployee/service/EmployeeService.java
@@ -0,0 +1,53 @@
+package com.example.testemployee.service;
+
+import com.example.testemployee.entity.Employee;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class EmployeeService {
+    List<Employee> employeeList = new ArrayList<>(Arrays.asList(
+            new Employee(1, "John", "Wa"),
+            new Employee(2, "Jack", "Ne")
+    ));
+
+    public List<Employee> getEmployees() {
+        return employeeList;
+    }
+
+    public Employee getEmployee(int employeeId) {
+        return employeeList.stream().filter(e -> (
+                e.getEmployeeId() == employeeId
+        )).findFirst().get();
+    }
+
+    public void addEmployee(Employee employee) {
+        employeeList.add(employee);
+    }
+
+    public void updateEmployee(Employee employee) {
+        List<Employee> tempList = new ArrayList<>();
+        for (Employee e : employeeList) {
+            if (e.getEmployeeId() == employee.getEmployeeId()) {
+                e.setEmployeeName(employee.getEmployeeName());
+                e.setEmployeeCity(employee.getEmployeeCity());
+            }
+            tempList.add(e);
+        }
+        this.employeeList = tempList;
+    }
+
+    public void deleteEmployee(int employeeId) {
+        List<Employee> tempList = new ArrayList<>();
+        for (Employee e : employeeList) {
+            if (e.getEmployeeId() == employeeId) {
+                continue;
+            }
+            tempList.add(e);
+        }
+        this.employeeList = tempList;
+    }
+}
