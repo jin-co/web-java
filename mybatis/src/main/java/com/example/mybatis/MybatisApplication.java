93ae1fc3f8431f9aa438e4bebeceeecf59efab3d
Kwangjin Baek <lilijk@hanmail.net>
2023-09-05 18:02:33 +0900

    Init

---
+++ mybatis/src/main/java/com/example/mybatis/MybatisApplication.java
@@ -0,0 +1,18 @@
+package com.example.mybatis;
+
+import com.example.mybatis.entity.User;
+import org.apache.ibatis.type.MappedTypes;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MappedTypes(User.class)
+@MapperScan("com.example.mybatis.mapper")
+@SpringBootApplication
+public class MybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisApplication.class, args);
+    }
+
+}
---
+++ mybatis/src/main/java/com/example/mybatis/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.example.mybatis.controller;
+
+import com.example.mybatis.entity.User;
+import com.example.mybatis.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/")
+    public List<User> getAll() {
+        return userMapper.findAll();
+    }
+}
