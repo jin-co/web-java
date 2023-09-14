4c609abb6aa4c8105869be04f6177fa8ba8fce77
jin-co <lilijk@hanmail.net>
2023-09-14 22:04:27 +0900

    Update

--- playground/src/main/java/com/example/playground/PlaygroundApplication.java
+++ playground/src/main/java/com/example/playground/PlaygroundApplication.java
@@ -8,16 +8,21 @@ public class PlaygroundApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PlaygroundApplication.class, args);
-        System.out.println(toMilesPerHour(0.25));
+        System.out.println(toMilesPerHour(-0.25));
     }
 
     public static long toMilesPerHour(double kilometersPerHour) {
         if (kilometersPerHour < 0) {
-            System.out.println(" =--- ");
             return -1L;
         } else {
             return Math.round(kilometersPerHour / 1.609);
         }
+    }
 
+    public static void printCoversion(double kilometersPerHour) {
+        if(kilometersPerHour < 0) {
+            System.out.println("Invalid Value");
+        }
+        return Math.round(kilometersPerHour * 1.609);
     }
 }
