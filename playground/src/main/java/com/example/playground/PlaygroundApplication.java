7231576fd9065f6bcf0c1413d11f4568c05705d2
jin-co <lilijk@hanmail.net>
2023-09-19 22:07:11 +0900

    Update

--- playground/src/main/java/com/example/playground/PlaygroundApplication.java
+++ playground/src/main/java/com/example/playground/PlaygroundApplication.java
@@ -76,7 +76,89 @@ public class PlaygroundApplication {
         if (summer && temperature >= 25 && temperature <= 45) {
             return true;
         }
+        return false;
+    }
+
+    public static String switchTest(String value) {
+        switch (value) {
+            case "1" -> {
+                System.out.println("good");
+                return "1";
+            }
+            case "2" -> {
+                System.out.println("bad");
+                return "2";
+            }
+            default -> {
+                System.out.println("What");
+                return "3";
+            }
+        }
+    }
+
+    public static void printNumberInWord(int number) {
+        switch (number) {
+            case 0:
+                System.out.println("ZERO");
+                break;
+            case 1:
+                System.out.println("ONE");
+                break;
+            case 2:
+                System.out.println("TWO");
+                break;
+            case 3:
+                System.out.println("THREE");
+                break;
+            case 4:
+                System.out.println("FOUR");
+                break;
+            case 5:
+                System.out.println("FIVE");
+                break;
+            case 6:
+                System.out.println("SIX");
+                break;
+            case 7:
+                System.out.println("SEVEN");
+                break;
+            case 8:
+                System.out.println("EIGHT");
+                break;
+            case 9:
+                System.out.println("NINE");
+                break;
+            default:
+                System.out.println("OTHER");
+                break;
+        }
+    }
+
+    public static boolean isLeapYear(int year) {
+        if(year < 1 || year > 9999) {
+            return false;
+        }
+
+        if (year % 400 == 0) {
+            return true;
+        }
+
+        if (year % 4 == 0 && year % 100 != 0) {
+            return true;
+        }
 
         return false;
+    }
+
+    public static int getDaysInMonth(int month, int year) {
+        if (month < 1 || month > 12) {
+            return -1;
+        }
+
+        if (year < 1 || year > 9999) {
+            return -1;
+        }
+
+
     }
 }
