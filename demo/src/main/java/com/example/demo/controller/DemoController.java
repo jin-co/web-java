e4209b3de14192df4bc7ebfcd2c3dc5a24f47d14
jin-co <lilijk@hanmail.net>
2023-10-01 20:33:08 +0900

    Update

--- demo/src/main/java/com/example/demo/controller/DemoController.java
+++ demo/src/main/java/com/example/demo/controller/DemoController.java
@@ -1,13 +1,17 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class DemoController {
 
+    @Value("${env.test}")
+    private String test;
+
     @GetMapping("/")
     public String main() {
-        return "Heyi";
+        return test;
     }
 }
