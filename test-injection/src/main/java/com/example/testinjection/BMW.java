1c24c63b00b03210e18d818aef4a1cb53f3f9d5f
jin-co <lilijk@hanmail.net>
2023-10-06 21:57:57 +0900

    Update

--- test-injection/src/main/java/com/example/testinjection/BMW.java
+++ test-injection/src/main/java/com/example/testinjection/BMW.java
@@ -1,9 +1,27 @@
 package com.example.testinjection;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class BMW implements Car {
+
+    public BMW() {
+    }
+
+    @PostConstruct
+    public void postConstruct() {
+
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+
+    }
 
     @Override
     public String getModel() {
