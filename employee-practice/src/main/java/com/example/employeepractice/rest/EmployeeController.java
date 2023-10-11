0f6829dc24b99d7a8d28e73cdd16c5c10924f223
jin-co <lilijk@hanmail.net>
2023-10-11 22:01:06 +0900

    Update

--- employee-practice/src/main/java/com/example/employeepractice/rest/EmployeeController.java
+++ employee-practice/src/main/java/com/example/employeepractice/rest/EmployeeController.java
@@ -4,9 +4,7 @@ import com.example.employeepractice.dao.EmployeeDAO;
 import com.example.employeepractice.entity.Employee;
 import com.example.employeepractice.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -29,5 +27,37 @@ public class EmployeeController {
     @GetMapping("/employees")
     public List<Employee> findAll() {
         return employeeService.findAll();
+    }
+
+    @GetMapping("/employees/{id")
+    public Employee getEmployee(@PathVariable int id) {
+        Employee employee = employeeService.findById(id);
+        if (employee == null) {
+            throw new RuntimeException("not found");
+        }
+        return employee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee employee) {
+        Employee employee1 = employeeService.save(employee);
+        employee1.setId(0);
+        return employee1;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee employee) {
+        Employee employee1 = employeeService.save(employee);
+        return employee1;
+    }
+
+    @DeleteMapping("employees/{id}")
+    public String deleteEmployee(@PathVariable int id) {
+        Employee employee = employeeService.findById(id);
+        if (employee == null) {
+            throw new RuntimeException("not exist");
+        }
+        employeeService.delete(id);
+        return "deleted";
     }
 }
