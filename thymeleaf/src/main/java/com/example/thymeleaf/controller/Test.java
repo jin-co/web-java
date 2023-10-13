fdd62af697061e05a06de42f7ba3c61836887bc3
Kwangjin Baek <lilijk@hanmail.net>
2023-10-13 18:15:45 +0900

    Update

---
+++ thymeleaf/src/main/java/com/example/thymeleaf/controller/Test.java
@@ -0,0 +1,7 @@
+package com.example.thymeleaf.controller;
+
+public class Test {
+    EAN_UOM	String	10	YES	New Field to be added
+    EAN	String	20	YES	New Field to be added
+    MATERIAL	Active_status	String	2	NO
+}
