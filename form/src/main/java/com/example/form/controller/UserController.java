1cae41b904ad520167eaacd1d4ce8c9e53fa9c3a
jin-co <lilijk@hanmail.net>
2023-10-22 21:25:02 +0900

    Completed form

--- form/src/main/java/com/example/form/controller/UserController.java
+++ form/src/main/java/com/example/form/controller/UserController.java
@@ -1,19 +1,34 @@
 package com.example.form.controller;
 
 import com.example.form.entity.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 public class UserController {
+    @Value("${genders}")
+    private List<String> genders;
+
+    @Value("${levels}")
+    private List<String> levels;
+
+    @Value("${hobbies}")
+    private List<String> hobbies;
 
     @GetMapping("/userForm")
     public String displayForm(Model model) {
         User user = new User();
         model.addAttribute("user", user);
+
+        model.addAttribute("genders", genders);
+        model.addAttribute("levels", levels);
+        model.addAttribute("hobbies", hobbies);
         return "user-form";
     }
 
--- form/src/main/java/com/example/form/entity/User.java
+++ form/src/main/java/com/example/form/entity/User.java
@@ -1,10 +1,39 @@
 package com.example.form.entity;
 
+import java.util.List;
+
 public class User {
     private String firstName;
     private String lastName;
+    private String gender;
+    private String level;
+    private List<String> hobbies;
 
     public User() {
+    }
+
+    public List<String> getHobbies() {
+        return hobbies;
+    }
+
+    public void setHobbies(List<String> hobbies) {
+        this.hobbies = hobbies;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
     }
 
     public String getFirstName() {
